b2f917882a96933e1cb09c0242607d2101b2a8a0
sassssafraaaas <sallyli@eduroam126-22.hampshire.edu>
2017-04-15 00:51:58 -0400

    User Database

---
+++ app/src/main/java/UserDatabaseHelper/UserDatabaseHelper.java
@@ -0,0 +1,9 @@
+package UserDatabaseHelper;
+
+/**
+ * Created by sallyli on 4/15/17.
+ */
+
+public class UserDatabaseHelper {
+
+}
---
+++ app/src/main/java/Users/User.java
@@ -0,0 +1,31 @@
+package Users;
+
+/**
+ * Created by sallyli on 4/15/17.
+ */
+
+public class User {
+    private String name;
+    private int questionID;
+    private int answer; //Option 1 = 1, Option 2 = 2
+
+    public User(String name, Integer questionID, Integer answer){
+        this.name = name;
+        this.questionID = questionID;
+        this.answer = answer;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getQUestion(){
+        return questionID;
+    }
+
+    public int answer(){
+        return answer;
+    }
+
+
+}
